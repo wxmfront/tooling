0f6ac107af1411ebb7ff4037dd07fc09ec55b429
EGOIST <0x142857@gmail.com>
2016-04-27 00:13:09 +0800

    update webpack-hot-server

--- commands/watch.js
+++ commands/watch.js
@@ -1,12 +1,16 @@
 'use strict'
+const cwd = require('cwd')
 const opn = require('opn')
 const server = require('webpack-hot-server')
 const loadConfig = require('../lib/loadConfig')
 
 module.exports = function watch(options) {
   const config = loadConfig('watch', options)
-  server({ config, port: options.port, customIndex: true })
-    .then(port => {
+  server({
+    config,
+    port: options.port,
+    customIndex: cwd('build')
+  }).then(port => {
       if (!options.silent) {
         opn(`http://localhost:${port}`)
       }
